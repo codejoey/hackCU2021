136e89229039c9bd4d8449863b804d6386489166
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-03-06 23:34:54 +0300

    onboarding

--- app/src/screens/login.js
+++ app/src/screens/login.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Image, Button, Vibration } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Svg, { Line } from 'react-native-svg';
-import { Icon } from 'react-native-elements';
- 
+import GoogleFit, { Scopes } from 'react-native-google-fit'
+import {ButtonGroup, Input, Icon} from 'react-native-elements';
+
 
 
 
@@ -21,28 +21,111 @@ export default function Login() {
       ? 'wait 1s, vibrate 2s, wait 3s'
       : 'wait 1s, vibrate, wait 2s, vibrate, wait 3s';
 
+    // GoogleFit.checkIsAuthorized().then(() => {
+    //     console.log(GoogleFit.isAuthorized) // Then you can simply refer to `GoogleFit.isAuthorized` boolean.
+    // })
+
+ 
+    const buttons = ['Login', 'Sign Up']
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    
+      
+
        
 
    
     return (
         <View style={styles.container}>
-             <Image source={require('../assets/logo.png')} style={{width:200, height:200, resizeMode:'contain', alignSelf:'center', marginTop:'10%'}}></Image>
-            <View style={{ marginTop: '-10%' ,borderTopLeftRadius:20, borderTopRightRadius:20, height:'110%',backgroundColor: 'rgba(62, 62, 62, 0.6)' }}>
+             <Image source={require('../assets/logo.png')} style={{width:200, height:200, resizeMode:'contain', alignSelf:'center', marginTop:'20%'}}></Image>
+            <View style={{ marginTop: '-10%' ,borderTopLeftRadius:20, borderTopRightRadius:20, height:'110%' }}>
                
-                <Text style={{
-                    fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 30, textAlign: 'center', color:'#FFF', marginTop:'5%', 
-                }}>Sign in to continue</Text>
-                
-           
+            <ButtonGroup
+                onPress={setSelectedIndex}
+                selectedIndex={selectedIndex}
+                buttons={buttons}
+                selectedButtonStyle={{backgroundColor:'#36A044'}}
+                containerStyle={{height: 30, borderColor:'#36A044', width:'50%', alignSelf:'center', marginTop:'15%', backgroundColor:'#FFF8F1'}}
+            />
+                                    
+           {selectedIndex==0 &&
             <View style={{paddingHorizontal:'10%', marginTop:'10%',paddingTop:'5%', height:100}}>
             
-                <TextInput placeholder="E-mail" placeholderTextColor="#FFF" style={{fontSize:20, borderWidth:2, borderRadius:5, borderColor:'#DDD', width:'70%', paddingLeft:'5%', alignSelf:'center', paddingVertical:'2.5%'}}></TextInput>
-                <TextInput placeholder="Password" secureTextEntry placeholderTextColor="#FFF" style={{fontSize:20, borderWidth:2, borderRadius:5, borderColor:'#DDD', width:'70%', paddingLeft:'5%', paddingVertical:'2.5%', marginTop:'5%', alignSelf:'center'}}></TextInput>
+            <Input
+                placeholder='   Email'
+                style={{backgroundColor:'#FFF', borderRadius:20}}
+                leftIcon={
+                    <Icon
+                    name='envelope'
+                    type='font-awesome'
+                    size={24}
+                    color='black'
+                    />
+                }
+            />  
+            <Input
+                placeholder='   Password'
+                style={{backgroundColor:'#FFF', borderRadius:20}}
+                leftIcon={
+                    <Icon
+                    name='lock'
+                    type='font-awesome'
+                    size={24}
+                    color='black'
+                    />
+                }
+                secureTextEntry
+            />              
 
-            </View>
+            </View>}
+
+            {selectedIndex==1 &&
+            <View style={{paddingHorizontal:'10%', marginTop:'10%',paddingTop:'5%', height:100}}>
+            
+            <Input
+                placeholder='   Email'
+                style={{backgroundColor:'#FFF', borderRadius:20}}
+                leftIcon={
+                    <Icon
+                    name='envelope'
+                    type='font-awesome'
+                    size={24}
+                    color='black'
+                    />
+                }
+            />  
+             <Input
+                placeholder='   Code'
+                style={{backgroundColor:'#FFF', borderRadius:20}}
+                leftIcon={
+                    <Icon
+                    name='user'
+                    type='font-awesome'
+                    size={24}
+                    color='black'
+                    />
+                }
+            /> 
+            <Input
+                placeholder='   Password'
+                style={{backgroundColor:'#FFF', borderRadius:20}}
+                leftIcon={
+                    <Icon
+                    name='lock'
+                    type='font-awesome'
+                    size={24}
+                    color='black'
+                    />
+                }
+                secureTextEntry
+            />              
+
+            </View>}
+
+
             <View style={{alignSelf:'center', marginTop:'10%'}}>
-           <Text onPress={() => Vibration.vibrate(10 * ONE_SECOND_IN_MS)} style={{backgroundColor:'#16B65C', color:'#FFF', textAlignVertical:'center', fontWeight:'bold', 
-           fontSize:15, textAlign:'center', paddingVertical:'3.5%', paddingHorizontal:'10%', borderRadius:5, marginTop:'20%'}}>SIGN IN</Text>
+           <Text onPress={() => Vibration.vibrate(10 * ONE_SECOND_IN_MS)} style={{backgroundColor:'#36A044', color:'#FFF', textAlignVertical:'center', fontWeight:'bold', 
+           fontSize:15, textAlign:'center', paddingVertical:'2.5%', paddingHorizontal:'10%', borderRadius:20, marginTop:'25%'}}>Submit</Text>
             </View>
             </View>
         </View>
@@ -54,7 +137,7 @@ const styles = StyleSheet.create({
     container: {
         height: '100%',
         position: 'relative',
-        backgroundColor: '#16B65C'
+        backgroundColor: '#FFF8F1'
     },
     header: {
         height: '50%',
