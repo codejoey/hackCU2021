d555a239fe560f7491523140e221d87db5a1317e
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-03-07 10:18:05 +0300

    audio, emotion, health

---
+++ app/src/screens/health.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, Button, Vibration, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import GoogleFit, { Scopes } from 'react-native-google-fit'
+import {ButtonGroup, Input, Icon} from 'react-native-elements';
+import { Audio } from 'expo-av';
+
+
+
+export default function Health() {
+    const navigation = useNavigation();
+    const [mood, setMood] = useState('nervous');
+  
+
+    return (
+        <View style={styles.container}>
+            <Text style={{backgroundColor:'#2193D1', height:100, color:'#FFF', fontWeight:'bold', 
+            textAlign:'center', paddingTop:'10%', fontSize:20}}>My Health</Text>
+            <TouchableOpacity><Text style={{backgroundColor:'#B5D0DE', height:50, color:'#FFF', fontWeight:'900', 
+            textAlign:'center', paddingTop:'3.5%', fontSize:20}}>View History</Text></TouchableOpacity>
+
+       
+            <View style={{paddingHorizontal:'10%', marginTop:'10%',paddingTop:'5%', flexDirection:'row', flexWrap:'wrap', alignSelf:'center'}}>
+            <View style={{borderRadius:20, borderWidth:2,borderColor:'#2193D1', height:100, width:'45%', padding:'7.5%', marginRight:'2.5%'}}>
+                <Icon name="pulse" type="fontisto" color={"#2193D1"} size={20}></Icon>
+                <Text style={{fontWeight:'bold', fontSize:10, textAlign:'center', color:'#2193D1'}}>Pulse Rate</Text>
+                <Text style={{fontWeight:'bold', fontSize:20, textAlign:'center', color:'#2193D1'}}>50</Text>
+            </View>
+            <View style={{borderRadius:20, borderWidth:2,borderColor:'#2193D1', height:100, width:'45%', padding:'7.5%'}}>
+                <Icon name="shoe-prints" type="font-awesome-5" color={"#2193D1"} size={20}></Icon>
+                <Text style={{fontWeight:'bold', fontSize:10, textAlign:'center', color:'#2193D1'}}>Steps</Text>
+                <Text style={{fontWeight:'bold', fontSize:20, textAlign:'center', color:'#2193D1'}}>50</Text>
+            </View>
+            <View style={{borderRadius:20, borderWidth:2,borderColor:'#2193D1', height:100, width:'92%', padding:'7.5%', marginTop:'5%'}}>
+                <Icon name="brain" type="font-awesome-5" color={"#2193D1"} size={20}></Icon>
+                <Text style={{fontWeight:'bold', fontSize:10, textAlign:'center', color:'#2193D1'}}>Stress Levels</Text>
+                <Text style={{fontWeight:'bold', fontSize:20, textAlign:'center', color:'#2193D1'}}>50</Text>
+            </View>
+
+
+            
+            </View>
+            <View style={{alignSelf:'center', marginTop:'10%'}}>
+              
+            </View>
+    
+            <View style={{backgroundColor:'#FFF', height:50, position:'absolute', bottom:0, borderTopRightRadius:10, 
+            borderTopLeftRadius:10, width:'100%', paddingHorizontal:'15%', flexDirection:'row', paddingVertical:'2.5%'}}>
+              <TouchableOpacity onPress={()=>{navigation.navigate('Record')}}><Text style={{marginRight:'20%'}}><Icon name="home" type="feather" color={'#A9A5A5'}></Icon></Text></TouchableOpacity>
+              <TouchableOpacity onPress={()=>{navigation.navigate('Emotion')}}><Text style={{marginRight:'20%'}}><Icon name="smile-o" type="font-awesome" color={'#A9A5A5'}></Icon></Text></TouchableOpacity>
+              <TouchableOpacity onPress={()=>{navigation.navigate('Health')}}><Text style={{marginRight:'20%'}}><Icon name="barschart" type="ant-design" color={'#2193D1'}></Icon></Text></TouchableOpacity>
+              <TouchableOpacity><Text style={{marginRight:'20%'}}><Icon name="settings" type="feather" color={'#A9A5A5'}></Icon></Text></TouchableOpacity>
+
+            </View>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        position: 'relative',
+        backgroundColor: '#FFF8F1'
+    },
+    header: {
+        height: '50%',
+        width: '60%',
+        marginTop: '10%',
+        resizeMode: 'contain',
+        alignSelf: 'center'
+    },
+
+});
